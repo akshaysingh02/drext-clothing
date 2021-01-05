f427283ae4c26d2520f190281b4104700a92496f
Akshay singh <ww.akshay@gmail.com>
2021-01-05 22:54:31 +0530

    Added signup with email capabalities from the firebase utils and added the signup form too

--- src/App.js
+++ src/App.js
@@ -21,8 +21,22 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged( async user => {
-      createUserProfileDocument(user)
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot((snapShot) => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          })
+          console.log(this.state)
+        });
+      } else {
+        this.setState({ currentUser: userAuth})
+      }
     });
   }
 
@@ -33,7 +47,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Header currentUser = {this.state.currentUser}/>
+        <Header currentUser={this.state.currentUser} />
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route exact path="/shop" component={ShopPage} />
--- src/pages/sign-up-and-sign-in/sign-up-and-sign-in.component.jsx
+++ src/pages/sign-up-and-sign-in/sign-up-and-sign-in.component.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import './sign-up-and-sign-in.style.scss';
 
 import SignIn from '../../components/sign-in/sign-in.component'
+import SignUp from '../../components/sign-up/sign-up.component'
 
 const SignUpAndSignIn = () => (
     <div className='sign-in-and-sign-up'>
         <SignIn />
+        <SignUp />
     </div>
 )
 
